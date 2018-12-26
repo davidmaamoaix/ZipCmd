1cd1b643bce59ea0dd16e9fc4a6a4ba9a4cf8de5
David Ma <davidmaamoaix@gmail.com>
2018-12-26 10:53:50 +0800

    Finishing touches

--- script.js
+++ script.js
@@ -16,8 +16,30 @@ function addCell() {
 }
 
 function compile(){
+	var entries = Array.from(document.getElementsByClassName('entry'));
+	entries = entries.map(function(entry) {
+		return entry.value;
+	});
+
+	// Cleanup cmds.
+	entries.push('setblock ~ ~1 ~ command_block 0 0 {Command:\\"fill ~ ~-3 ~ ~ ~1 ~ air\\"}');
+	entries.push('setblock ~ ~2 ~ redstone_block');
+	entries.push('kill @e[type=commandblock_minecart,r=1]');
+
+	// Wrapping cmds.
+	entries = entries.map(function(entry) {
+		return `{id:commandblock_minecart,Command:\"${entry}\"}`;
+	});
+
+	var out_cmd = entries.join(',');
+
+	// Wrapping entire cmd.
+	out_cmd = 'summon falling_block ~ ~1 ~ {Block:glass,Time:1,Passengers:[{\
+		id:falling_block,Block:redstone_block,Time:1,Passengers:[{\
+		id:falling_block,Block:activator_rail,Time:1,Passengers:[' + out_cmd + ']}]}]}'
+
 	var output = window.open();
-	output.document.write('<p>It worked!</p>');
+	output.document.write(`<p>${out_cmd}</p>`);
 }
 
 function htmlNode(html) {
